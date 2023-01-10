be2fb52f83d84799c5c360fac50877c89bc3bebd
Alimata28 <117599863+Alimata28@users.noreply.github.com>
2023-01-10 18:48:20 +0300

    Add files via upload

---
+++ odev last lab 2.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+
+int main() {
+    int m,n,i,j, a, b, f[100][100];
+    int v[3][3];
+	int g[100][100];
+	int sum, sum1;
+	sum1 = 0;
+    
+    printf("Enter number of line>");
+    scanf("%d", &m);
+    printf("\nEnter number of row>");
+    scanf("%d", &n); 
+    printf("Matrix F (random numbers[0,250])\n");
+    srand(time(NULL));
+    for (i = 0; i < m; i++) {
+    	for(j=0; j< n; j++){
+    		f[i][j] = rand() % 250 + 1;
+            printf("f[%d][%d]==> %d\n", i+1, j+1, f[i][j]);
+            
+		}    
+    }
+    printf("Enter the values of matrix v\n");
+    for(i=0; i<3; i++){
+    	for(j=0; j<3; j++){
+    	    printf("v[%d][%d]==> %d\n", i+1,j+1, v[i][j]);
+    	    scanf("%d", &v[i][j]);
+		}
+	}
+	if(m>2 && n>2){
+    	int x=0, b=3;
+    	while(x!= m && b != n){
+    	    for(i=x;i<b;i++){
+    	   	    for(j=x;j<b;j++){
+    	   	    	sum1= sum1 + (f[i][j] * v[i-1][j-1]);
+				}
+		    }
+	        sum= sum1;
+	        printf("g[%d][%d]==> %d\n", x-3, b-3, sum);
+	        b++;
+            x++;
+		}
+	    m++;		
+	} 
+		 	
+	else{
+		printf("The matrix cannot be filter!");
+	}
+
+    return 0;  
